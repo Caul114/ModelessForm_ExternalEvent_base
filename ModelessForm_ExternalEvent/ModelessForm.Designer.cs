35c3c5f9fbc472164264c882d481a3b8e03a709f
Luca Della Bianca <lucadellabianca4@gmail.com>
2021-01-07 14:29:26 +0100

    Add project files.

---
+++ ModelessForm_ExternalEvent/ModelessForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ModelessForm_ExternalEvent
+{
+    partial class ModelessForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uploadExcelOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(1229, 595);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 0;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click_1);
+            // 
+            // ModelessForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1316, 630);
+            this.Controls.Add(this.exitButton);
+            this.MaximizeBox = false;
+            this.Name = "ModelessForm";
+            this.Text = "BOLD Sample";
+            this.Load += new System.EventHandler(this.ModelessForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private OpenFileDialog uploadExcelOpenFileDialog;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Button exitButton;
+    }
+}
